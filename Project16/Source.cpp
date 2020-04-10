7369b8915f54c67a42a5a3d385b72c864a49c350
dmitrithedwarf <dmitriventro@gmail.com>
2020-04-10 19:27:43 +0700

    tt_cmm_2

--- Project16/Source.cpp
+++ Project16/Source.cpp
@@ -47,3 +47,53 @@ void pole_view(char** pole) //показывает поле.
 }
 
 
+void player(char** pole) //ход игрока.
+{
+	int x, y;
+	cout << "Выберите клетку\nСтрока:"; cin >> x;
+	cout << "Столбец: "; cin >> y;
+	x--; y--;
+	if (x < 0 || y < 0 || x>2 || y>2 || pole[x][y] != ' ')
+	{
+		cout << "Ошибка\n";
+		player(pole);
+	}
+	else pole[x][y] = 'X';
+}
+
+
+void compuhter(char** pole) //ход компухтера.
+{
+	int i, j;
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+			if (pole[i][j] == ' ') break;
+		if (pole[i][j] == ' ') break; //нужно, чтобы выйти из цикла по i.
+	}
+	if (i * j == 9)
+	{
+		cout << "GAME OVER\n";
+		exit(0);
+	}
+	else
+		pole[i][j] = 'O';
+}
+
+
+char who_is_the_winner(char** pole)
+{
+	int i;
+	for (i = 0; i < 3; i++)
+		if (pole[i][0] == pole[i][1] && pole[i][0] == pole[i][2]) //проверка строк.
+			return pole[i][0];
+	for (i = 0; i < 3; i++)
+		if (pole[0][i] == pole[1][i] && pole[0][i] == pole[2][i]) //проверка столбцов.
+			return pole[0][i];
+	if (pole[0][0] == pole[1][1] && pole[1][1] == pole[2][2]) //проверка главной диагонали.
+		return pole[0][0];
+	if (pole[0][2] == pole[1][1] && pole[1][1] == pole[2][0]) //проверка второстепенной диагонали.
+		return pole[0][2];
+	return ' ';
+}
+
