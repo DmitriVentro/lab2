0de52fd3ea6275d970e0ee386bcefdb7483e2f8b
dmitrithedwarf <dmitriventro@gmail.com>
2020-04-10 19:31:03 +0700

    ft_cmm_1

--- Project16/Source.cpp
+++ Project16/Source.cpp
@@ -1,99 +1,134 @@
 #include <iostream>
-using std::cout;
-using std::cin;
-using std::endl;
-void pole_view(char** pole);
-void player(char** pole);
-void compuhter(char** pole);
-char who_is_the_winner(char** pole);
-int main()
-{
-	setlocale(LC_ALL, "Russian");
-	char round;
-	round = ' ';
-	cout << "Это игра Крестики-Нолики\nВы будете играть против компьютера\n";
-	char** pole;
-	pole = new char* [3];
-	for (int i = 0; i < 3; i++)
-		pole[i] = new char[3];
-	for (int i = 0; i < 3; i++)
-		for (int j = 0; j < 3; j++)
-			pole[i][j] = ' ';
-	do
+#include <math.h>
+using namespace std;
+namespace first_task {
+	void enter_values(int& rows, int& cols, int& a, int& b)
 	{
-		pole_view(pole);
-		player(pole);
-		round = who_is_the_winner(pole);
-		if (round != ' ')
-			break;
-		compuhter(pole);
-		round = who_is_the_winner(pole);
-	} while (round == ' ');
-	if (round == 'X')
-		cout << "Поздравляем, вы победили!\n";
-	else
-		cout << "Вас победил компьютер... Теперь живите с этим.\n";
-	pole_view(pole);
-	return 0;
-}
-void pole_view(char** pole) //показывает поле.
-{
-	for (int i = 0; i < 3; i++)
-	{
-		cout << pole[i][0] << " | " << pole[i][1] << " | " << pole[i][2];
-		if (i != 2) cout << "\n \n";
+		cout << "Левая граница значений = ";
+		cin >> a;
+		cout << "Правая граница значений = ";          // Левая и правая граница значений в матрице
+		cin >> b;
+		cout << "Сколько столбцов X строк = ";
+		cin >> rows;
+		cout << "\n";
+		cols = rows;                        //Количество столбиков в матрице
 	}
-	cout << endl;
-}
-
-
-void player(char** pole) //ход игрока.
-{
-	int x, y;
-	cout << "Выберите клетку\nСтрока:"; cin >> x;
-	cout << "Столбец: "; cin >> y;
-	x--; y--;
-	if (x < 0 || y < 0 || x>2 || y>2 || pole[x][y] != ' ')
+	void show_arr(int** arr, int rows, int cols)
 	{
-		cout << "Ошибка\n";
-		player(pole);
+		cout << "Матрица:\n";
+
+		for (int i = 0; i < rows; i++)
+		{
+			arr[i] = new int[cols];
+		}
 	}
-	else pole[x][y] = 'X';
-}
-
-
-void compuhter(char** pole) //ход компухтера.
-{
-	int i, j;
-	for (i = 0; i < 3; i++)
+	void fill_arr(int** arr, int rows, int cols, int a, int b)
 	{
-		for (j = 0; j < 3; j++)
-			if (pole[i][j] == ' ') break;
-		if (pole[i][j] == ' ') break; //нужно, чтобы выйти из цикла по i.
+		for (int i = 0; i < rows; i++)
+		{
+			for (int j = 0; j < cols; j++)
+			{
+				arr[i][j] = a + rand() % (b - a + 1);
+				cout << arr[i][j] << "\t";
+			}
+			cout << endl;
+		}
 	}
-	if (i * j == 9)
+	void red_area_analyze(int** arr, int rows)
 	{
-		cout << "GAME OVER\n";
-		exit(0);
+		cout << "Выше главной и побочной диагональ:";
+		for (int i = 0; i < rows; i++)
+		{
+			for (int j = i + 1; j < rows - i - 1; j++)
+			{
+				cout << arr[i][j] << " ";
+			}
+		}
+		cout << endl;
+		system("pause");
 	}
-	else
-		pole[i][j] = 'O';
-}
+	void red_area_show_element(int** arr, int rows, int& min, int& imin, int& jmin)
+	{
+		cout << "Минимальный положительный элемент: ";
+		for (int i = 0; i < rows; i++)
+		{
+			for (int j = i + 1; j < rows - i - 1; j++)
+			{
 
+				if (arr[i][j] > 0)
+				{
+					if (arr[i][j] < min)
+					{
+						min = arr[i][j];
+						imin = i;
+						jmin = j;
+					}
+				}
+			}
+		}
+		if (min > 0 && min != INT_MAX)
+		{
+			cout << min << "\n";; //Вывод минимального положительного числа выше гланой диагонали
+			system("pause");
+		}
+		else {
+			cout << "There aren`t positive elements\n";
+		}
 
-char who_is_the_winner(char** pole)
-{
-	int i;
-	for (i = 0; i < 3; i++)
-		if (pole[i][0] == pole[i][1] && pole[i][0] == pole[i][2]) //проверка строк.
-			return pole[i][0];
-	for (i = 0; i < 3; i++)
-		if (pole[0][i] == pole[1][i] && pole[0][i] == pole[2][i]) //проверка столбцов.
-			return pole[0][i];
-	if (pole[0][0] == pole[1][1] && pole[1][1] == pole[2][2]) //проверка главной диагонали.
-		return pole[0][0];
-	if (pole[0][2] == pole[1][1] && pole[1][1] == pole[2][0]) //проверка второстепенной диагонали.
-		return pole[0][2];
-	return ' ';
-}
+	}
+	void green_area_analyze(int** arr, int cols)
+	{
+		cout << "Ниже побочной и главной:\n";
+
+		for (int i = 1; i <= cols - 1; i++)
+			for (int j = cols - i; j < cols - 1; ++j)
+				if (i > j)
+					cout << arr[i][j] << " ";
+	}
+	void green_area_show_element(int** arr, int cols, int a, int& max, int& imax, int& jmax)
+	{
+		cout << "Максимальный отрицательный: ";
+
+		for (int i = 1; i <= cols - 1; i++)
+		{
+			for (int j = cols - i; j < cols - 1; ++j)
+			{
+				if (i > j)
+				{
+					if (arr[i][j] < 0)
+					{
+						if (arr[i][j] > max)
+						{
+
+							max = arr[i][j];
+							imax = i;
+							jmax = j;
+						}
+					}
+				}
+			}
+		}
+		if (max < 0 && max != a) {
+			cout << max << "\n";; //Вывод максимального отрицательного
+
+		}
+		else {
+			cout << "Отрицательных нету\n";
+
+		}
+		system("pause");
+	}
+	void modification(int** arr, int cols, int a, int max, int min, int jmax, int imax, int imin, int jmin, int rows)
+	{
+		if (max < 0 && max != a && min > 0 && min != INT_MAX) {
+			cout << "Новая матрица: \n";
+			int extra;
+			extra = arr[imin][jmin];
+			arr[imin][jmin] = arr[imax][jmax];
+			arr[imax][jmax] = extra;
+			for (int i = 0; i < rows; i++)
+			{
+				for (int j = 0; j < cols; j++)
+				{
+					cout << arr[i][j] << "\t";
 
