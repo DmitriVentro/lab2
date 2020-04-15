076afe75afaa739722cd98a6e44829b138855cc8
dmitrithedwarf <dmitriventro@gmail.com>
2020-04-15 13:04:21 +0700

    gl2_cmm_2

--- Project16/Source.cpp
+++ Project16/Source.cpp
@@ -4,13 +4,132 @@
 #include <random>
 #include <Windows.h>
 using namespace std;
-void fV(int** a, int n, int m);
-int lP(int** a, int n, int m, int s, int de);
-void nbh(int nb[][2], int x, int y);
-int aLNB(int** a, int n, int m, int x, int y);
-void NG(int** a, int** a1, int n, int m);
-int FC(int** a, int** a1, int n, int m, int x, int s, int su, int de, int det);
-void CC(int** a, int** a1, int n, int m, int s, int su, int de, int det);
+namespace game
+{
+	void fV(int** a, int n, int m)
+	{
+		//if (n <= 30 && m <= 30)
+		//{
+		for (int i = 0; i < n; i++)
+		{
+			for (int j = 0; j < m; j++)
+			{
+				{
+					if (a[i][j] == 1)
+						cout << '+';
+					else
+						cout << ' ';
+					cout << '|';
+				}
+			}
+			cout << endl;
+
+		}
+		cout << endl << "---------" << endl;
+		//}
+		//else
+		cout << endl << "---------" << endl;
+	}
+
+	int lP(int** a, int n, int m, int s, int de) // сумма всех живых и мертвых клеток
+	{
+		int count = 0, sum = 0;
+		for (int i = 0; i < n; i++)
+			for (int j = 0; j < m; j++)
+				if (a[i][j] == 1)
+				{
+					count++;
+					sum++;
+					s++;
+				}
+		de = n * m - sum;
+		cout << "Живых клеток - " << sum << "   Мёртвых клеток - " << de << endl;
+		return count;
+	}
+
+	void nbh(int nb[][2], int x, int y)
+	{
+		int k = 0;
+		for (int i = x - 1; i <= x + 1; i++)
+			for (int j = y - 1; j <= y + 1; j++)
+			{
+				if (i == x && j == y)
+					continue;
+				nb[k][0] = i;
+				nb[k][1] = j;
+				k++;
+			}
+	}
+
+	int aLNB(int** a, int n, int m, int x, int y)
+	{
+		int count = 0;
+		int nb[8][2];
+		int x_, y_;
+		nbh(nb, x, y);
+		for (int i = 0; i < 8; i++)
+		{
+			x_ = nb[i][0];
+			y_ = nb[i][1];
+			if (x_ < 0 || y_ < 0)
+				continue;
+			if (x_ >= n || y_ >= m)
+				continue;
+			if (a[x_][y_] == 1)
+				count++;
+		}
+		return count;
+	}
+
+	void NG(int** a, int** a1, int n, int m)
+	{
+		int livingNB, p;
+		for (int i = 0; i < n; i++)
+			for (int j = 0; j < m; j++)
+			{
+				p = a1[i][j];
+				livingNB = aLNB(a1, n, m, i, j);
+				if (p == 0)
+				{
+					if (livingNB == 3)
+						a[i][j] = 1;
+				}
+				else
+					if (livingNB < 2 || livingNB > 3)
+						a[i][j] = 0;
+			}
+	}
+
+	void CC(int** a, int** a1, int n, int m, int s, int su, int de, int det) //Копирование старого 
+	{
+		unsigned int i, j;
+		for (i = 0; i < n; i++) {
+			for (j = 0; j < m; j++) {
+				a1[i][j] = a[i][j];
+				su = s;
+				det = de;
+			}
+		}
+	}
+	int FC(int** a, int** a1, int n, int m, int x, int s, int su, int de, int det)  //Проверка на живых и цикл
+	{
+		unsigned int i, j;
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < m; j++)
+			{
+				if ((a[i][j] != a1[i][j]) || ((s == su) || (de == det)))
+				{
+
+
+					return -1;
+
+				}
+			}
+		}
+		return 0;
+	}
+}
 int main()
 {
 	setlocale(LC_ALL, "ru");
@@ -44,12 +163,12 @@ int main()
 	bool death = false;
 	do
 	{
-		fV(field, n, m);
-		Edem = FC(field, PreviousField, n, m, x, s, su, de, det) == 0;
-		death = FC(field, PreviousField, n, m, x, s, su, de, det) == -2;
-		CC(field, PreviousField, n, m, s, su, de, det);
-		NG(field, PreviousField, n, m);
-		lp = lP(field, n, m, s, de);
+		game::fV(field, n, m);
+		Edem = game::FC(field, PreviousField, n, m, x, s, su, de, det) == 0;
+		death = game::FC(field, PreviousField, n, m, x, s, su, de, det) == -2;
+		game::CC(field, PreviousField, n, m, s, su, de, det);
+		game::NG(field, PreviousField, n, m);
+		lp = game::lP(field, n, m, s, de);
 		if (Edem)
 		{
 			cout << "Эдем создан\n";
@@ -65,126 +184,3 @@ int main()
 	return 0;
 }
 
-void fV(int** a, int n, int m)
-{
-	//if (n <= 30 && m <= 30)
-	//{
-	for (int i = 0; i < n; i++)
-	{
-		for (int j = 0; j < m; j++)
-		{
-			{
-				if (a[i][j] == 1)
-					cout << '+';
-				else
-					cout << ' ';
-				cout << '|';
-			}
-		}
-		cout << endl;
-
-	}
-	cout << endl << "---------" << endl;
-	//}
-	//else
-	cout << endl << "---------" << endl;
-}
-
-int lP(int** a, int n, int m, int s, int de) // сумма всех живых и мертвых клеток
-{
-	int count = 0, sum = 0;
-	for (int i = 0; i < n; i++)
-		for (int j = 0; j < m; j++)
-			if (a[i][j] == 1)
-			{
-				count++;
-				sum++;
-				s++;
-			}
-	de = n * m - sum;
-	cout << "Живых клеток - " << sum << "   Мёртвых клеток - " << de << endl;
-	return count;
-}
-
-void nbh(int nb[][2], int x, int y)
-{
-	int k = 0;
-	for (int i = x - 1; i <= x + 1; i++)
-		for (int j = y - 1; j <= y + 1; j++)
-		{
-			if (i == x && j == y)
-				continue;
-			nb[k][0] = i;
-			nb[k][1] = j;
-			k++;
-		}
-}
-
-int aLNB(int** a, int n, int m, int x, int y)
-{
-	int count = 0;
-	int nb[8][2];
-	int x_, y_;
-	nbh(nb, x, y);
-	for (int i = 0; i < 8; i++)
-	{
-		x_ = nb[i][0];
-		y_ = nb[i][1];
-		if (x_ < 0 || y_ < 0)
-			continue;
-		if (x_ >= n || y_ >= m)
-			continue;
-		if (a[x_][y_] == 1)
-			count++;
-	}
-	return count;
-}
-
-void NG(int** a, int** a1, int n, int m)
-{
-	int livingNB, p;
-	for (int i = 0; i < n; i++)
-		for (int j = 0; j < m; j++)
-		{
-			p = a1[i][j];
-			livingNB = aLNB(a1, n, m, i, j);
-			if (p == 0)
-			{
-				if (livingNB == 3)
-					a[i][j] = 1;
-			}
-			else
-				if (livingNB < 2 || livingNB > 3)
-					a[i][j] = 0;
-		}
-}
-
-void CC(int** a, int** a1, int n, int m, int s, int su, int de, int det) //Копирование старого 
-{
-	unsigned int i, j;
-	for (i = 0; i < n; i++) {
-		for (j = 0; j < m; j++) {
-			a1[i][j] = a[i][j];
-			su = s;
-			det = de;
-		}
-	}
-}
-int FC(int** a, int** a1, int n, int m, int x, int s, int su, int de, int det)  //Проверка на живых и цикл
-{
-	unsigned int i, j;
-	for (i = 0; i < n; i++)
-	{
-		for (j = 0; j < m; j++)
-		{
-			if ((a[i][j] != a1[i][j]) || ((s == su) || (de == det)))
-			{
-
-
-				return -1;
-
-			}
-		}
-	}
-	return 0;
-}
