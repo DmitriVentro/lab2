e033d63e848c3e0f06a2954acbb51a639e228d3d
dmitrithedwarf <dmitriventro@gmail.com>
2020-04-15 12:56:41 +0700

    gl2_cmm_1

--- Project16/Source.cpp
+++ Project16/Source.cpp
@@ -1,54 +1,31 @@
 #include <iostream>
 #include <cstdlib>
 #include <cstdio>
-#include <ctime>
 #include <random>
 #include <Windows.h>
-using std::cout;
-using std::cin;
-using std::endl;
-void MENU();                                                       //меню
-void fieldView(int** a, int n, int m);                             //показывание поля
-int livingPoints(int** a, int n, int m);                           //живые точки
-void neighborhood(int nb[][2], int x, int y);                      //соседние точки
-int amountLivingNB(int** a, int n, int m, int x, int y);           //кол-во живых соседей
-void NextGen(int** a, int** a1, int n, int m);                     //следующее поколение точек
-void CopyCopy(int** a, int** a1, int n, int m);                    //копирование прошлого поля
-int FieldsCompare(int** a, int** a1, int n, int m);                //сравнение полей
+using namespace std;
+void fV(int** a, int n, int m);
+int lP(int** a, int n, int m, int s, int de);
+void nbh(int nb[][2], int x, int y);
+int aLNB(int** a, int n, int m, int x, int y);
+void NG(int** a, int** a1, int n, int m);
+int FC(int** a, int** a1, int n, int m, int x, int s, int su, int de, int det);
+void CC(int** a, int** a1, int n, int m, int s, int su, int de, int det);
 int main()
 {
-	int n, m, key;
-	MENU();
-	cout << endl << ":"; cin >> key;
-	srand(time(NULL)); //выбор способа задания размера поля
-	do
-		switch (key)
-		{
-		case 1:
-			n = rand() % 21;
-			m = rand() % 21;
-			break;
-		case 2:
-			cout << "Введите n и m\n";
-			cin >> n;
-			cin >> m;
-			break;
-		case 0:
-			cout << "Ещё увидимся!\n";
-			break;
-		default:
-			cout << "ошибка!\n";
-			break;
-		}
-	while (n < 4 || m < 4);
-	cout << "\nРазмерность Вселенной: " << n << " строки " << m << " колонки\n";
-	int** field; //задаем поле (многомерный массив)
+	setlocale(LC_ALL, "ru");
+	int n, m, x = 0, s = 0, su = -50, de = 0, det = -50;
+	cout << "Задайте размерность поле :";
+	cin >> n;
+	cin >> m;
+	cout << "\nРазмер поля: " << n << " строк и " << m << " столбцов\n";
+	int** field;
 	field = new int* [n];
 	for (int i = 0; i < n; i++)
 		field[i] = new int[m];
-	std::random_device rd; //заполнение случайно точками
-	std::mt19937 gen(rd());
-	std::uniform_int_distribution<> dis(1, 10000);
+	random_device rd;
+	mt19937 gen(rd());
+	uniform_int_distribution<> dis(1, 10000);
 	for (int i = 0; i < n; i++)
 		for (int j = 0; j < m; j++)
 		{
@@ -58,65 +35,78 @@ int main()
 			else
 				field[i][j] = 0;
 		}
-	int** PreviousField; //задаем поле для запоминания предыдущего поколения
+	int** PreviousField;
 	PreviousField = new int* [n];
 	for (int i = 0; i < n; i++)
 		PreviousField[i] = new int[m];
-	int livingpoints = -1;
+	int lp = -1;
 	bool Edem = false;
+	bool death = false;
 	do
 	{
-		fieldView(field, n, m);                                   //показ поля
-		CopyCopy(field, PreviousField, n, m);                     //копируем поле
-		NextGen(field, PreviousField, n, m);                      //генерируем новое
-		Edem = FieldsCompare(field, PreviousField, n, m) == 0;    //сравниваем поля
-		livingpoints = livingPoints(field, n, m);                 //проверяем количество живых клеток
+		fV(field, n, m);
+		Edem = FC(field, PreviousField, n, m, x, s, su, de, det) == 0;
+		death = FC(field, PreviousField, n, m, x, s, su, de, det) == -2;
+		CC(field, PreviousField, n, m, s, su, de, det);
+		NG(field, PreviousField, n, m);
+		lp = lP(field, n, m, s, de);
 		if (Edem)
-			cout << "Эдемов сад\n";                              //Эдемов сад
-		if (livingpoints == 0)
-			cout << "ВЫ ПРОИГРАЛИ ДА ВСЕ МЕРТВЫ\n";                  //все сдохли. конец.
-		Sleep(2000); //прерывание цикла, чтобы пользователь успел увидеть поле
-	} while (livingpoints != 0 || !Edem);
+		{
+			cout << "Эдем создан\n";
+			return 0;
+		}
+		if (lp == 0 || death)
+		{
+			cout << "Все мертвы\n";
+			return 0;
+		}
+		Sleep(100);
+	} while (lp != 0 || !Edem);
 	return 0;
 }
 
-void MENU() //меню
+void fV(int** a, int n, int m)
 {
-	cout << "\t!ЗДРАСТВУЙТЕ!\n";
-	cout << "Есть только два выбора:\n";
-	cout << endl;
-	cout << "1 - Рандомное заполнение\n";
-	cout << "2 - Или на выбор\n";
-	cout << "0 - exit\n";
-}
-
-void fieldView(int** a, int n, int m) //вывод поля
-{
+	//if (n <= 30 && m <= 30)
+	//{
 	for (int i = 0; i < n; i++)
 	{
 		for (int j = 0; j < m; j++)
 		{
-			if (a[i][j] == 1)
-				cout << '*';
-			else
-				cout << ' ';
-			cout << ' ';
+			{
+				if (a[i][j] == 1)
+					cout << '+';
+				else
+					cout << ' ';
+				cout << '|';
+			}
 		}
 		cout << endl;
+
 	}
+	cout << endl << "---------" << endl;
+	//}
+	//else
+	cout << endl << "---------" << endl;
 }
 
-int livingPoints(int** a, int n, int m) //счет живых точек
+int lP(int** a, int n, int m, int s, int de) // сумма всех живых и мертвых клеток
 {
-	int count = 0;
+	int count = 0, sum = 0;
 	for (int i = 0; i < n; i++)
 		for (int j = 0; j < m; j++)
 			if (a[i][j] == 1)
+			{
 				count++;
+				sum++;
+				s++;
+			}
+	de = n * m - sum;
+	cout << "Живых клеток - " << sum << "   Мёртвых клеток - " << de << endl;
 	return count;
 }
 
-void neighborhood(int nb[][2], int x, int y) //координаты соседей точки
+void nbh(int nb[][2], int x, int y)
 {
 	int k = 0;
 	for (int i = x - 1; i <= x + 1; i++)
@@ -130,12 +120,12 @@ void neighborhood(int nb[][2], int x, int y) //координаты соседей точки
 		}
 }
 
-int amountLivingNB(int** a, int n, int m, int x, int y) //кол-во живых соседей у клетки[x][y]
+int aLNB(int** a, int n, int m, int x, int y)
 {
 	int count = 0;
 	int nb[8][2];
 	int x_, y_;
-	neighborhood(nb, x, y);
+	nbh(nb, x, y);
 	for (int i = 0; i < 8; i++)
 	{
 		x_ = nb[i][0];
@@ -150,14 +140,14 @@ int amountLivingNB(int** a, int n, int m, int x, int y) //кол-во живых соседей у
 	return count;
 }
 
-void NextGen(int** a, int** a1, int n, int m) //следующее поколение точек
+void NG(int** a, int** a1, int n, int m)
 {
 	int livingNB, p;
 	for (int i = 0; i < n; i++)
 		for (int j = 0; j < m; j++)
 		{
 			p = a1[i][j];
-			livingNB = amountLivingNB(a1, n, m, i, j);
+			livingNB = aLNB(a1, n, m, i, j);
 			if (p == 0)
 			{
 				if (livingNB == 3)
@@ -169,18 +159,32 @@ void NextGen(int** a, int** a1, int n, int m) //следующее поколение точек
 		}
 }
 
-void CopyCopy(int** a, int** a1, int n, int m) //копирование прошлого поля
+void CC(int** a, int** a1, int n, int m, int s, int su, int de, int det) //Копирование старого 
 {
-	for (int i = 0; i < n; i++)
-		for (int j = 0; j < m; j++)
+	unsigned int i, j;
+	for (i = 0; i < n; i++) {
+		for (j = 0; j < m; j++) {
 			a1[i][j] = a[i][j];
+			su = s;
+			det = de;
+		}
+	}
 }
-
-int FieldsCompare(int** a, int** a1, int n, int m) //сравнение полей
+int FC(int** a, int** a1, int n, int m, int x, int s, int su, int de, int det)  //Проверка на живых и цикл
 {
-	for (int i = 0; i < n; i++)
-		for (int j = 0; j < m; j++)
-			if (a != a1)
+	unsigned int i, j;
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < m; j++)
+		{
+			if ((a[i][j] != a1[i][j]) || ((s == su) || (de == det)))
+			{
+
+
 				return -1;
+
+			}
+		}
+	}
 	return 0;
 }
